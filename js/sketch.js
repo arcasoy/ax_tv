acfb66f96878689e5aba1b6b91176331c33dc147
Buntry <parkergriep@yahoo.com>
2017-12-27 22:23:50 -0500

    implement p5 canvas

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  console.log("p5 loaded");
+
+  let width = 0.5 * displayWidth;
+  let height = 0.8 * displayHeight;
+  let canvas = createCanvas(width, height);
+  canvas.parent("circle-menu");
+
+
+  console.log("p5 setup complete");
 }
 
 function draw() {
-
+  background(255);
 }
