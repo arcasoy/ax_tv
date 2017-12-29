aa1709b2d3b43ab7189eb6f72ecbe5a2d9a1ba78
Buntry <parkergriep@yahoo.com>
2017-12-29 01:01:44 -0500

    implement working movingAngles

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,13 @@
 let balls;
+let len;
+let increment;
 let wheelCenter;
 let radius;
 
 let angle = 0;
 let omega = 0;
+let desiredAngle = angle;
+let moving = false;
 
 function setup() {
 
@@ -13,6 +17,11 @@ function setup() {
   canvas.parent("circle-menu");
 
   balls = createBalls();
+  len = balls.length;
+  increment = 2 * PI / len;
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].angle = 2 * PI - i * increment;
+  }
 
   wheelCenter = createVector(40, (height / 2));
   radius = 250;
@@ -25,12 +34,10 @@ function draw() {
 
   drawWheel();
   wheelUpdate();
-  console.log(angle, omega);
 }
 
 function drawWheel() {
-  let len = balls.length;
-  let increment = 2 * PI / len;
+
   for (let i = 0; i < balls.length; i++) {
     let x = radius * cos((i * increment) + angle);
     let y = radius * sin((i * increment) + angle);
@@ -43,11 +50,41 @@ function drawWheel() {
 }
 
 function mouseWheel(event) {
+  moving = false;
   let c = 0.001;
   omega += c * event.delta;
+}
+
+function mouseClicked() {
+  for (let i = 0; i < balls.length; i++) {
+    if (balls[i].hovered()) {
+      moving = true;
+      desiredAngle = balls[i].angle;
+    }
+  }
 }
 
 function wheelUpdate() {
   angle += omega;
   omega *= 0.3;
+  if (moving) {
+    moveToAngle(desiredAngle);
+  }
+  if (angle < 0) {
+    angle += 2 * PI;
+  }
+}
+
+function moveToAngle(desiredAngle) {
+  if (angle - desiredAngle > PI) {
+    angle = lerp(angle - 2 * PI, desiredAngle, 0.1);
+  } else if (desiredAngle - angle > PI) {
+    angle = lerp(angle, desiredAngle - 2 * PI, 0.1);
+  } else {
+    angle = lerp(angle, desiredAngle, 0.1);
+  }
+
+  if (abs(desiredAngle - angle) < 0.02) {
+    moving = false;
+  }
 }
